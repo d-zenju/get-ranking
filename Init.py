17d101565761f4d30cddd68281c7ff3eb4ce2007
d-zenju <yoshizumi@zoho.com>
2016-11-22 08:27:23 +0900

    update

--- Init.py
+++ Init.py
@@ -4,11 +4,11 @@ from bs4 import BeautifulSoup
 
 # class : for read config file
 class Config:
-    def __init__(self, filepass):
-        self.filepass = filepass
+    def __init__(self, filepath):
+        self.filepath = filepath
 
     def read(self):
-        config = BeautifulSoup(open(self.filepass), 'lxml')
+        config = BeautifulSoup(open(self.filepath), 'lxml')
         self.yahooKey = config.yahoo.appid.contents[0]
         self.rakutenKey = config.rakuten.applicationid.contents[0]
         self.amazonKey = {
@@ -16,3 +16,4 @@ class Config:
             'secretaccesskey':config.amazon.secretaccesskey.contents[0],
             'associatetag':config.amazon.associatetag.contents[0]
             }
+        self.category = config.category.filepath.contents[0];
